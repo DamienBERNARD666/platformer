33bfb10f9ae3121d2541a3d70b47efb82f09c46f
DamienBERNARD666 <56837799+DamienBERNARD666@users.noreply.github.com>
2021-01-20 11:50:09 +0100

    add animation system

--- main.py
+++ main.py
@@ -13,12 +13,8 @@ screen = pygame.display.set_mode(WINDOW_SIZE, 0,32)
 
 display = pygame.Surface((300, 200))
 
-
-
-player_image = pygame.image.load('assets/player.png')
-player_image.set_colorkey((255,255,255))
-
 grass_image = pygame.image.load('assets/grass.PNG')
+
 TILE_SIZE = grass_image.get_width()
 dirt_image = pygame.image.load('assets/dirt.PNG')
 
@@ -41,6 +37,46 @@ def load_map(path):
     for row in data:
         game_map.append(list(row))
     return game_map
+
+global animation_frames
+
+animation_frames = {}
+
+
+def load_animation(path, frame_duration):
+    global animation_frames
+    animation_name = path.split('/')[-1]
+    animation_frame_data = []
+    n = 0
+    for frame in frame_duration:
+        animation_frame_id = animation_name + '_' + str(n)
+        image_location = path + '/' + animation_frame_id + '.png'
+        animation_image = pygame.image.load(image_location).convert()
+        animation_image.set_colorkey((255, 255, 255))
+        animation_frames[animation_frame_id] = animation_image.copy()
+        for i in range(frame):
+            animation_frame_data.append(animation_frame_id)
+        n+=1
+        return animation_frame_data
+
+
+def change_action(action_var, frame, new_value):
+    if action_var !=  new_value:
+        action_var = new_value
+        frame = 0
+    return action_var, frame
+
+animation_database  = {}
+
+animation_database['run'] = load_animation("player_animations/run", [7,7])
+animation_database['idle'] = load_animation("player_animations/idle", [7,7,40])
+
+player_action = 'idle'
+player_frame = 0
+player_flip = False
+
+
+
 
 game_map = load_map('map')
 
@@ -74,7 +110,7 @@ def move(rect, movement, tiles):
     return rect, collision_types
 
 
-player_rect = pygame.Rect(50,50, player_image.get_width(), player_image.get_height())
+player_rect = pygame.Rect(100,100,5,13)
 
 background_objects = [[0.25,[120,10,70,400]],[0.25,[280,30,40,400]],[0.5,[30,40,40,400]],[0.5,[130,90,100,400]],[0.5,[300,80,120,400]]]
 
@@ -122,6 +158,18 @@ while True:
     if player_y_momentum > 3:
         player_y_momentum = 3
 
+    if player_movement[0] > 0:
+        player_action, player_frame = change_action(player_action, player_frame, 'run')
+        player_flip = False
+
+    if player_movement[0] == 0:
+        player_action, player_frame = change_action(player_action, player_frame, 'idle')
+        player_flip = True
+
+    if player_movement[0] < 0:
+        player_action, player_frame = change_action(player_action, player_frame, 'run')
+        player_flip = True
+
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
 
     if collisions['bottom'] or collisions['top']:
@@ -130,7 +178,14 @@ while True:
     else:
         air_timer += 1
 
-    display.blit(player_image, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
+
+
+    player_frame += 1
+    if player_frame >= len(animation_database[player_action]):
+        player_frame = 0
+    player_img_id = animation_database[player_action][player_frame]
+    player_image = animation_frames[player_img_id]
+    display.blit(pygame.transform.flip(player_image, player_flip, False), (player_rect.x - scroll[0], player_rect.y - scroll[1]))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
